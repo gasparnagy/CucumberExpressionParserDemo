818d93765398564c4f8a7091d4d99318674ae372
Gaspar Nagy <gaspar.nagy@gmail.com>
2020-05-26 22:40:25 +0200

    Use BERP 1.2.0

--- generate-parser.cmd
+++ generate-parser.cmd
@@ -1,4 +1,4 @@
-@set BERP_VERSION=1.1.1
+@set BERP_VERSION=1.2.0
 @set BERP_INSTALL=%USERPROFILE%\.nuget\packages\berp\%BERP_VERSION%\tools\net471
 
-%BERP_INSTALL%\berp.exe -t %BERP_INSTALL%\GeneratorTemplates\CSharp.razor -g CucumberExpressionsGrammar.berp -o CucumberExpressionParserDemo\Parser\Parser.generated.cs
+%BERP_INSTALL%\berp.exe -t %BERP_INSTALL%\GeneratorTemplates\CSharp.razor -g CucumberExpressionsGrammar.berp -o CucumberExpressionParserDemo\Parser\Parser.generated.cs %*
